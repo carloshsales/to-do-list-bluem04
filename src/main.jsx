b5eb5cb427f0ae5b91ef7e1b544cb61f0a00de3b
Carlos Henrique <carlosh.sales@outlook.com>
2023-01-18 17:41:53 -0300

    change name style file

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
-import { GlobalStyle } from "./styles";
+import { GlobalStyle } from "./styles/global.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
